91f7dbb7119925316bc80953c4a0d541f0f658a0
HasinduLanka <35098511+HasinduLanka@users.noreply.github.com>
2019-08-01 17:42:02 +0530

    minFix

---
+++ LankaStocks/Shared.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LankaStocks
+{
+
+    public static class Error
+    {
+        public static string[] Errors = { "Feild Cannot Be Empty!", "Feild Must Be A Number!" };
+
+        private static bool ShowError(string txt, Control ctrl)
+        {
+            ctrl.BackColor = Color.FromKnownColor(KnownColor.Red);
+            //Core.Log(txt);
+           // MessageBox.Show(txt, "LankaStocks - Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return false;
+        }
+
+        public static bool Num(Control Ctrl)
+        {
+            if (!string.IsNullOrWhiteSpace(Ctrl.Text))
+            {
+                if (float.TryParse(Ctrl.Text, out float A))
+                {
+                    Ctrl.BackColor = Color.FromKnownColor(KnownColor.WindowFrame);
+                    return true;
+                }
+                else
+                {
+                    //Ctrl.Text = Ctrl.Text.Substring(0, Ctrl.Text.Length - 1);
+                    //Ctrl.Select();
+                    return ShowError(Errors[1], Ctrl);
+                }
+            }
+            else return ShowError(Errors[0], Ctrl);
+        }
+
+        public static bool Txt(Control Ctrl)
+        {
+            if (!string.IsNullOrWhiteSpace(Ctrl.Text))
+            {
+                Ctrl.BackColor = Color.FromKnownColor(KnownColor.WindowFrame);
+                return true;
+            }
+            else return ShowError(Errors[0], Ctrl);
+        }
+    }
+
+}
--- LankaStocks/Statics.cs
+++ LankaStocks/Statics.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using LankaStocks.UIForms;
 
 
 namespace LankaStocks
@@ -18,59 +17,5 @@ namespace LankaStocks
         }
     }
 
-    public static class Forms
-    {
-        public static Login login;
-        public static Dashboard dashboard;
-        public static AddData addData;
-        public static AddItems addItems;
-        public static ItemIntake itemIntake;
-        public static FrmanageData frmanageData;
-        public static FrmanageItems frmanageItems;
-        public static FrmSales frmSales;
-        public static FrmTransaction frmTransaction;
-        public static FrmSettings frmSettings;
-    }
-
-    public static class Error
-    {
-        public static string[] Errors = { "Feild Cannot Be Empty!", "Feild Must Be A Number!" };
-
-        private static bool ShowError(string txt, Control ctrl)
-        {
-            ctrl.BackColor = Color.FromKnownColor(KnownColor.Red);
-            MessageBox.Show(txt, "LankaStocks - Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            return false;
-        }
-
-        public static bool Num(Control Ctrl)
-        {
-            if (!string.IsNullOrWhiteSpace(Ctrl.Text))
-            {
-                if (float.TryParse(Ctrl.Text, out float A))
-                {
-                    Ctrl.BackColor = Color.FromKnownColor(KnownColor.WindowFrame);
-                    return true;
-                }
-                else
-                {
-                    //Ctrl.Text = Ctrl.Text.Substring(0, Ctrl.Text.Length - 1);
-                    //Ctrl.Select();
-                    return ShowError(Errors[1], Ctrl);
-                }
-            }
-            else return ShowError(Errors[0], Ctrl);
-        }
-
-        public static bool Txt(Control Ctrl)
-        {
-            if (!string.IsNullOrWhiteSpace(Ctrl.Text))
-            {
-                Ctrl.BackColor = Color.FromKnownColor(KnownColor.WindowFrame);
-                return true;
-            }
-            else return ShowError(Errors[0], Ctrl);
-        }
-    }
 }
 
