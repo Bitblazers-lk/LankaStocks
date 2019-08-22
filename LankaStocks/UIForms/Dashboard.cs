b2c949ca35f301d4727a421249bbc365cdc8e015
HasinduLanka <35098511+HasinduLanka@users.noreply.github.com>
2019-08-22 20:38:05 +0530

    Fixed FrmIOS butt reduncted

--- LankaStocks/UIForms/Dashboard.cs
+++ LankaStocks/UIForms/Dashboard.cs
@@ -32,9 +32,7 @@ namespace LankaStocks.UIForms
             this.panel1.BackColor = RemoteDBs.Settings.commonSettings.Get.MenuColor;
             this.panel2.BackColor = RemoteDBs.Settings.commonSettings.Get.MenuColor;
 
-            FrmIosButton b = new FrmIosButton();
-            b.Show();
-
+           
             cm.MenuItems.Add("Open Quick Sell Window", new EventHandler(Open_QC_Window_Click));
             btnIssueItem.ContextMenu = cm;
 
