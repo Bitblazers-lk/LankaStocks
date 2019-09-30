fb42174388abbbe24082375a8079e8254332f831
hariwij <53245949+hariwij@users.noreply.github.com>
2019-09-30 19:13:29 +0530

    upoid

--- LankaStocks.dev/Program.cs
+++ LankaStocks.dev/Program.cs
@@ -16,263 +16,60 @@ namespace LankaStocks.dev
         {
             Core.IsDebug = true;
 
-            Draw1();
-            Draw();
+            Bitmap maplab = new Bitmap(Properties.Resources.Capture, new Size(237, 30));
+            DrawPic(maplab, true);
 
+            Bitmap mapli = new Bitmap(Properties.Resources._4, new Size(230, 100));
+            DrawPic(mapli);
+
+            Bitmap mapd = new Bitmap(Properties.Resources._5, new Size(230, 100));
+            DrawPic(mapd);
+
+            Bitmap mapq = new Bitmap(Properties.Resources._9, new Size(230, 100));
+            DrawPic(mapq);
+
+            Bitmap map = new Bitmap(Properties.Resources.Capture2, new Size(237, 20));
+            DrawPic(map);
+
+            Bitmap map2 = new Bitmap(Properties.Resources._8, new Size(237, 100));
+            DrawPic(map2);
+
+            Bitmap map1 = new Bitmap(Properties.Resources.Capture1, new Size(237, 20));
+            DrawPic(map1);
+
+            Console.WriteLine("\n\n\nPress Enter To Continue : Hasi");
+           Console.ReadLine();
             LankaStocks.Program.Main();
         }
-        static void Draw()
+
+        static void DrawPic(Bitmap myBitmap, bool Invert = false)
         {
-            Console.WriteLine(@" _______             _______                                                              ");
-            Console.WriteLine(@"|       |           |       |                                       ------------------    ");
-            Console.WriteLine(@"|       |           |       |                                      /  ---------------     ");
-            Console.WriteLine(@"|       |           |       |               _______               /  /                    ");
-            Console.WriteLine(@"|       |           |       |              |       |             /  /                     ");
-            Console.WriteLine(@"|       |           |       |              |       |            |  |                      ");
-            Console.WriteLine(@"|       |___________|       |       _______|       |_______     |  |                      ");
-            Console.WriteLine(@"|                           |      |                       |    |  |          _________   ");
-            Console.WriteLine(@"|                           |      |                       |    |  |         |______  |   ");
-            Console.WriteLine(@"|        ___________        |      |_______         _______|    |  |                | |   ");
-            Console.WriteLine(@"|       |           |       |              |       |            |  |                | |   ");
-            Console.WriteLine(@"|       |           |       |              |       |             \  \               | |   ");
-            Console.WriteLine(@"|       |           |       |              |_______|              \  \              | |   ");
-            Console.WriteLine(@"|       |           |       |                                      \  \             | |   ");
-            Console.WriteLine(@"|       |           |       |                                       \  \------------| |   ");
-            Console.WriteLine(@"|_______|           |_______|                                        \----------------|   ");
-            Console.WriteLine("                                             Harindu + Galaaaaaaaa                        \n \n \n");
-            Console.WriteLine(
-"                    \n" +
-"   ---------        \n" +
-"    Harindu         \n" +
-"   ---------        \n" +
-"                    \n" +
-"             .-----.\r\n" +
-"            /       `\\\r\n" +
-"          _|_         |\r\n" +
-"         /   \\        | \r\n" +
-"         '==='        |\r\n" +
-"         . ' .        |\r\n" +
-"        . : ' .       |\r\n" +
-"           '.         |\r\n" +
-"       . '    .       |\r\n" +
-"        .-\"\"\"-.       |\r\n" +
-"       /  \\___ \\      |\r\n" +
-"       |/`    \\|      |\r\n" +
-"       (  a  a )      |\r\n" +
-"       |   _\\ |       |\r\n" +
-"       )\\  =  /       |\r\n" +
-"   _.-'  '---;        |\r\n" +
-" /`           `-.     |\r\n" +
-"|                \\    |\r\n" +
-"|    |   .  & .   \\   |\r\n" +
-"\\    /      &   |  ;  |\r\n" +
-"|   |           |  ;  |\r\n" +
-"|   /\\          /  |  |\r\n" +
-"\\   \\ )   -:-  /\\  \\  |\r\n" +
-" `.  `-.  -:-  | \\  \\_|\r\n" +
-"   '-.  `-.    (  './\\`\\\r\n" +
-"    / `'-. `\\  |    \\/_/\r\n" +
-"    |    \\  |  |      |\r\n" +
-"    |    /'-\\  /      |\r\n" +
-"     \\   \\   | |      |\r\n" +
-"      \\   )_/\\ |      |\r\n" +
-"       \\      \\|      |\r\n" +
-"        \\      \\      |\r\n" +
-"         '.     |     |\r\n" +
-"           /   /      |\r\n" +
-"          /  .';      |\r\n" +
-"        /`  /  |      |\r\n" +
-"       /   /   |      |\r\n" +
-" jgs  |  .' \\  |      |\r\n" +
-"      /  \\  )  |      |\r\n" +
-"      \\   \\ /  '-.._  |\r\n" +
-"       '.ooO\\__._.Ooo |\r\n \n");
-
-
-
-
-
-            Console.WriteLine(" \n \n  Galaa In the next door \n \n" +
-"                       ,ad8888888888888888ba,\r\n" +
-"                     ad88888888888888888888888a,\r\n" +
-"                   a88888\\\"8888888888888888888888,\r\n" +
-"                ,8888\"\\ \\ \\\"P88888888888888888888b,\r\n" +
-"                d88 \\ \\       `\"\"P88888888888888888,\r\n" +
-"              ,8888b               \"\"88888888888888,\r\n" +
-"              d8P'''  ,aa,              \"\"888888888b\r\n" +
-"              888bbdd888888ba,  ,I         \"88888888,\r\n" +
-"              8888888888888888ba8\"         ,88888888b\r\n" +
-"             ,888888888888888888b,        ,8888888888\r\n" +
-"             (88888888888888888888,      ,88888888888,\r\n" +
-"             d888888888888888888888,    ,8   \"8888888b\r\n" +
-"             88888888888888888888888  .;8'\"\"\"  (888888\r\n" +
-"             8888888888888I\"8888888P ,8\" ,aaa,  888888\r\n" +
-"             888888888888I:8888888\" ,8\"  'b8d'  (88888\r\n" +
-"             (8888888888I'888888P' ,8) \\         88888\r\n" +
-"              88888888I\"  8888P'  ,8\")  \\        88888\r\n" +
-"              8888888I'   888\"   ,8\" (   )       88888\r\n" +
-"              (8888I\"     \"88,  ,8\"             ,8888P\r\n" +
-"               888I'       \"P8 ,8\"   ____      ,88888)\r\n" +
-"              (88I'          \",8\"  M\"\"\"\"\"\"8888888'\r\n" +
-"             ,8I\"            ,8(    \"aaaa\"   ,8888888\r\n" +
-"         ,8I'             ,888a           ,8888888)\r\n" +
-"       ,8I'            ,888888,       ,888888888\r\n" +
-"    ,8I'             ,8888888'`-===-'888888888'\r\n" +
-" ,8I'            ,8888888\"        88888888\"\r\n" +
-" 8I'            ,8\"    88         \"888888P\r\n" +
-"8I            ,8'     88          `P888\"\r\n" +
-"8I           ,8I      88            \"8ba,.\r\n" +
-"(8,         ,8P'      88              88\"\"8bma,.\r\n" +
-" 8I        ,8P'       88,              \"8b   \"\"P8ma,\r\n" +
-" (8,      ,8d\"        `88,               \"8b     `\"8a\r\n" +
-"  8I     ,8dP         ,8X8,                \"8b.    :8b\r\n" +
-"  (8    ,8dP'  ,I    ,8XXX8,                `88,    8)\r\n" +
-"   8,   8dP'  ,I    ,8XxxxX8,     I,         8X8,  ,8\r\n" +
-"   8I   8P'  ,I    ,8XxxxxxX8,     I,        `8X88,I8\r\n" +
-"   I8,  \"   ,I    ,8XxxxxxxxX8b,    I,        8XXX88I,\r\n" +
-"   `8I      I'  ,8XxxxxxxxxxxxXX8    I        8XXxxXX8,\r\n" +
-"    8I     (8  ,8XxxxxxxxxxxxxxxX8   I        8XxxxxxXX8,\r\n" +
-"   ,8I     I[ ,8XxxxxxxxxxxxxxxxxX8  8        8XxxxxxxxX8,\r\n" +
-"   d8I,    I[ 8XxxxxxxxxxxxxxxxxxX8b 8       (8XxxxxxxxxX8,\r\n" +
-"   888I    `8,8XxxxxxxxxxxxxxxxxxxX8 8,     ,8XxxxxxxxxxxX8\r\n" +
-"   8888,    \"88XxxxxxxxxxxxxxxxxxxX8)8I    .8XxxxxxxxxxxxX8\r\n" +
-"  ,8888I     88XxxxxxxxxxxxxxxxxxxX8 `8,  ,8XxxxxxxxxxxxX8\"\r\n" +
-"  d88888     `8XXxxxxxxxxxxxxxxxxX8'  `8,,8XxxxxxxxxxxxX8\"\r\n" +
-"  888888I     `8XXxxxxxxxxxxxxxxX8'    \"88XxxxxxxxxxxxX8\"\r\n" +
-"  88888888bbaaaa88XXxxxxxxxxxxXX8)      )8XXxxxxxxxxXX8\"\r\n" +
-"  8888888I, ``\"\"\"\"\"\"8888888888888888aaaaa8888XxxxxXX8\"\r\n" +
-"  (8888888I,                      .  ```\"\"\"\"\"88888P\"\r\n" +
-"   88888888I,                   ,8I   8,       I8\"\r\n" +
-"    \"\"\"88888I,                ,8I'    \"I8,    ;8\"\r\n" +
-"           `8I,             ,8I'       `I8,   8)\r\n" +
-"            `8I,           ,8I'          I8  :8'\r\n" +
-"             `8I,         ,8I'           I8  :8\r\n" +
-"              `8I       ,8I'             `8  (8\r\n" +
-"               8I     ,8I'                8  (8;\r\n" +
-"               8I    ,8\"                  I   88,\r\n" +
-"              .8I   ,8'                       8\"8,\r\n" +
-"              (PI   '8                       ,8,`8,\r\n" +
-"             .88'            ,@           .a8X8,`8,\r\n" +
-"             (88             @@         ,a8XX888,`8,\r\n" +
-"            (888             @'       ,d8XX8\"  \"b `8,\r\n" +
-"           .8888,                     a8XXX8\"    \"a `8,\r\n" +
-"          .888X88                   ,d8XX8I\"      9, `8,\r\n" +
-"         .88:8XX8,                 a8XxX8I'       `8  `8,\r\n" +
-"        .88' 8XxX8a             ,ad8XxX8I'        ,8   `8,\r\n" +
-"        d8'  8XxxxX8ba,      ,ad8XxxX8I\"          8  ,  `8,\r\n" +
-"       (8I   8XxxxxxX888888888XxxxX8I\"            8  II  `8\r\n" +
-"       8I'   \"8XxxxxxxxxxxxxxxxxxX8I'            (8  8)   8;\r\n" +
-"      (8I     8XxxxxxxxxxxxxxxxxX8\"              (8  8)   8I\r\n" +
-"      8P'     (8XxxxxxxxxxxxxxX8I'                8, (8   :8\r\n" +
-"     (8'       8XxxxxxxxxxxxxxX8'                 `8, 8    8\r\n" +
-"     8I        `8XxxxxxxxxxxxX8'                   `8,8   ;8\r\n" +
-"     8'         `8XxxxxxxxxxX8'                     `8I  ,8'\r\n" +
-"     8           `8XxxxxxxxX8'                       8' ,8'\r\n" +
-"     8            `8XxxxxxX8'                        8 ,8'\r\n" +
-"     8             `8XxxxX8'                        d' 8'\r\n" +
-"     8              `8XxxX8                         8 8'\r\n" +
-"     8                \"8X8'                         \"8\"\r\n" +
-"     8,                `88     Love you              8\r\n" +
-"     8I                ,8'           Harindu        d)\r\n" +
-"     `8,               d8                          ,8\r\n" +
-"      (b               8'                         ,8'\r\n" +
-"       8,             dP                         ,8'\r\n" +
-"       (b             8'                        ,8'\r\n" +
-"        8,           d8                        ,8'\r\n" +
-"        (b           8'                       ,8'\r\n" +
-"         8,         a8       Gala            ,8'\r\n" +
-"         (b         8'        Gala          ,8'\r\n" +
-"          8,       ,8                      ,8'\r\n" +
-"          (b       8'                     ,8'\r\n" +
-"           8,     ,8                     ,8'\r\n" +
-"           (b     8'                    ,8'\r\n" +
-"            8,   d8                    ,8'\r\n" +
-"            (b  ,8'                   ,8'\r\n" +
-"             8,,I8                   ,8'\r\n" +
-"             I8I8'                  ,8'\r\n" +
-"             `I8I                  ,8'\r\n" +
-"              I8'                 ,8'\r\n" +
-"              \"8                 ,8'\r\n" +
-"              (8                ,8'\r\n" +
-"              8I               ,8'\r\n" +
-"              (b,   8,        ,8)\r\n" +
-"              `8I   \"88      ,8i8,\r\n" +
-"               (b,          ,8\"8\")\r\n" +
-"               `8I  ,8      8) 8 8\r\n" +
-"                8I  8I      \"  8 8\r\n" +
-"                (b  8I         8 8\r\n" +
-"                `8  (8,        b 8,\r\n" +
-"                 8   8)        \"b\"8,\r\n" +
-"                 8   8(         \"b\"8\r\n" +
-"                 8   \"I          \"b8,\r\n" +
-"                 8                `8)\r\n" +
-"                 8                 I8\r\n" +
-"                 8                 (8\r\n" +
-"                 8,                 8,\r\n" +
-"                 Ib                 8)\r\n" +
-"                 (8                 I8\r\n" +
-"                  8                 I8\r\n" +
-"                  8                 I8\r\n" +
-"                  8,                I8\r\n" +
-"                  Ib                8I\r\n" +
-"                  (8               (8'\r\n" +
-"                   8               I8\r\n" +
-"                   8,              8I\r\n" +
-"                   Ib             (8'\r\n" +
-"                   (8             I8\r\n" +
-"                   `8             8I\r\n" +
-"                    8            (8'\r\n" +
-"                    8,           I8\r\n" +
-"                    Ib           8I\r\n" +
-"                    (8           8'\r\n" +
-"                     8,         (8\r\n" +
-"                     Ib         I8\r\n" +
-"                     (8         8I\r\n" +
-"                     (8         8I\r\n" +
-"                      8,        8'\r\n" +
-"                      (b       (8\r\n" +
-"                       8,      I8\r\n" +
-"                       I8      I8\r\n" +
-"                       (8      I8\r\n" +
-"                        8      I8,\r\n" +
-"                        8      8 8,\r\n" +
-"                        8,     8 8'\r\n" +
-"                       ,I8     \"8\"\r\n" +
-"                      ,8\"8,     \"b\r\n" +
-"                     ,8' `8      8\r\n" +
-"                    ,8'   8      8,\r\n" +
-"                   ,8'    (a     \"b\r\n" +
-"                  ,8'     `8      (b\r\n" +
-"                  I8/      8       8,\r\n" +
-"                  I8-/     8       `8,\r\n" +
-"                  (8/-/    8        `8,\r\n" +
-"                   8I/-/  ,8         `8\r\n" +
-"                   `8I/--,I8        \\-8)\r\n" +
-"                    `8I,,d8I       \\-\\8)\r\n" +
-"                      \"bdI\"8,     \\-\\I8\r\n" +
-"                           `8,   \\-\\I8'\r\n" +
-"                            `8,,--\\I8'\r\n" +
-"                             `Ib,,I8'\r\n" +
-"                              `I8I'\r\n");
-        }
-
-        static void Draw1()
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(@"$#$                                #$#                $#$$#$             $#$    $#$       $#$                 #$#                      #$#$#$#$    $#$$#$$#$$#$$#$$#$$#$         $#$$#$                                                   ");
-            Console.WriteLine(@"$#$                               $#$#$               $#$ $#$            $#$    $#$      $#$                 $#$#$                   $#$#$#        $#$$#$$#$$#$$#$$#$$#$        $#$  $#$                                             ");
-            Console.WriteLine(@"$#$                              $#$ $#$              $#$  $#$           $#$    $#$     $#$                 $#$ $#$                 $#$#$                   $#$                $#$    $#$                          ");
-            Console.WriteLine(@"$#$                             $#$   $#$             $#$   $#$          $#$    $#$    $#$                 $#$   $#$               $#$#$                    $#$               $#$      $#$                                    ");
-            Console.WriteLine(@"$#$                            $#$     $#$            $#$    $#$         $#$    $#$   $#$                 $#$     $#$              $#$#$                    $#$              $#$        $#$                                 ");
-            Console.WriteLine(@"$#$                           $#$       $#$           $#$     $#$        $#$    $#$  $#$                 $#$       $#$              $#$#$                   $#$             $#$          $#$                                  ");
-            Console.WriteLine(@"$#$                          $#$         $#$          $#$      $#$       $#$    $#$ $#$                 $#$         $#$               $#$#$                 $#$            $#$            $#$                                    ");
-            Console.WriteLine(@"$#$                         $#$           $#$         $#$       $#$      $#$    $#$  $#$               $#$           $#$                $#$#$               $#$            $#$            $#$                              ");
-            Console.WriteLine(@"$#$                        $#$#$#$#$#$#$#$#$#$        $#$        $#$     $#$    $#$   $#$             $#$#$#$#$#$#$#$#$#$                $#$#$              $#$             $#$          $#$                                    ");
-            Console.WriteLine(@"$#$                       $#$#$#$#$#$#$#$#$#$#$       $#$         $#$    $#$    $#$    $#$           $#$#$#$#$#$#$#$#$#$#$                $#$#$             $#$              $#$        $#$                                          ");
-            Console.WriteLine(@"$#$                      $#$                 $#$      $#$          $#$   $#$    $#$     $#$         $#$                 $#$               $#$#$             $#$               $#$      $#$                                         ");
-            Console.WriteLine(@"$#$                     $#$                   $#$     $#$           $#$  $#$    $#$      $#$       $#$                   $#$             $#$#$              $#$                $#$    $#$                             ");
-            Console.WriteLine(@"$#$$#$$#$$#$$#$$#$$    $#$                     $#$    $#$            $#$ $#$    $#$       $#$     $#$                     $#$         $#$#$#                $#$                 $#$  $#$                                                       ");
-            Console.WriteLine(@"$#$$#$$#$$#$$#$$#$$   $#$                       $#$   $#$             $#$$#$    $#$        $#$   $#$                       $#$    $#$#$#$#                  $#$                  $#$$#$                         ");
+            bool black = false;
+            string s = "";
+            for (int y = 0; y < myBitmap.Height; y++)
+            {
+                for (int x = 0; x < myBitmap.Width; x++)
+                {
+                    Color pixelColor = myBitmap.GetPixel(x, y);
+                    if (pixelColor.R < 100 && pixelColor.G < 100 && pixelColor.B < 100) black = true;
+                    else black = false;
+                    if (black)
+                    {
+                        if (!Invert)
+                            s += "@";
+                        else s += " ";
+                    }
+                    else
+                    {
+                        if (!Invert)
+                            s += " ";
+                        else s += "@";
+                    }
+                }
+                s += "\n";
+            }
+            myBitmap.Dispose();
+            Console.WriteLine(s);
         }
     }
 }
